5175b2c6481c5cb646ad7db2996f9be2f8b86a66
Felipe Lopes <felipe.lopes@dca.ufrn.br>
2021-03-21 11:35:13 -0300

    chg: Add variantions on multiplication of dollar by constant

--- test_multi_currency_money.py
+++ test_multi_currency_money.py
@@ -4,3 +4,11 @@ def test_dolar_mult_by_constant():
     curret_value = dollar(5)
     curret_value.times(2)
     assert curret_value.amount == 10
+
+    curret_value = dollar(5)
+    curret_value.times(4)
+    assert curret_value.amount == 20
+
+    curret_value = dollar(10)
+    curret_value.times(7)
+    assert curret_value.amount == 70

b3ca5a65d877227b5dbbea012024e2063641d228
Felipe Lopes <felipe.lopes@dca.ufrn.br>
2021-03-21 16:00:35 -0300

    new: Add equals method

--- test_multi_currency_money.py
+++ test_multi_currency_money.py
@@ -1,5 +1,6 @@
 from multi_currency_money import dollar
 
+
 def test_dolar_mult_by_constant():
     curret_value = dollar(5)
     curret_value.times(2)
@@ -12,3 +13,9 @@ def test_dolar_mult_by_constant():
     curret_value = dollar(10)
     curret_value.times(7)
     assert curret_value.amount == 70
+
+
+def test_equal_same_currency():
+    assert dollar(5).equals(dollar(5))
+    assert dollar(12).equals(dollar(12))
+    assert not dollar(5).equals(dollar(6))

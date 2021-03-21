25dd517a35e8285cda8e414d73f271e7b2aecb9f
Felipe Lopes <felipe.lopes@dca.ufrn.br>
2021-03-21 11:33:24 -0300

    chg: Correct make times method generic as of the multiplier

--- multi_currency_money.py
+++ multi_currency_money.py
@@ -2,5 +2,5 @@ class dollar():
     def __init__(self, amount):
         self.amount = amount
 
-    def times(self, amount):
-        self.amount =  10
+    def times(self, multiplier):
+        self.amount *=  multiplier

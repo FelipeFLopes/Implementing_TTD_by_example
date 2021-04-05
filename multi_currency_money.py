accec113e6ae657c9f99a063410dc8e6d53859a4
Felipe Lopes <felipe.lopes@dca.ufrn.br>
2021-04-05 18:59:32 -0300

    chg: Refactor times code and only compare equal classes

--- multi_currency_money.py
+++ multi_currency_money.py
@@ -3,15 +3,24 @@ class money():
         self.amount = amount
 
     def equals(self, currency):
+
+        if type(self) != type(currency):
+            return False
+
         return self.amount == currency.amount
 
+
+class dollar(money):
     def times(self, multiplier):
         return dollar(self.amount * multiplier)
 
 
-class dollar(money):
-    pass
-
-
 class franc(money):
-    pass
+    def times(self, multiplier):
+        return franc(self.amount * multiplier)
+
+
+if __name__ == "__main__":
+    franc_value = franc(5)
+
+    test = franc_value.times()

91bf035e3e696819013c2374ddf1e86dfb6e5382
Felipe Lopes <felipe.lopes@dca.ufrn.br>
2021-04-05 20:58:59 -0300

    new: Add the notion of currency

--- multi_currency_money.py
+++ multi_currency_money.py
@@ -2,8 +2,9 @@ from abc import ABC, abstractmethod
 
 
 class money(ABC):
-    def __init__(self, amount):
+    def __init__(self, amount, currency):
         self.amount = amount
+        self._currency = currency
 
     def equals(self, currency):
 
@@ -12,24 +13,29 @@ class money(ABC):
 
         return self.amount == currency.amount
 
+    def currency(self):
+        return self._currency
+
     @abstractmethod
     def times(self, multiplier):
         pass
 
     @classmethod
     def dollar(self, amount):
-        return dollar(amount)
+        return dollar(amount, "USD")
 
     @classmethod
     def franc(self, amount):
-        return franc(amount)
+        return franc(amount, "CHF")
 
 
 class dollar(money):
+
     def times(self, multiplier):
-        return dollar(self.amount * multiplier)
+        return money.dollar(self.amount * multiplier)
 
 
 class franc(money):
+
     def times(self, multiplier):
-        return franc(self.amount * multiplier)
+        return money.franc(self.amount * multiplier)
--- test_multi_currency_money.py
+++ test_multi_currency_money.py
@@ -1,10 +1,16 @@
 from multi_currency_money import money
 
 
+def test_currency():
+    assert money.dollar(5).currency() == "USD"
+    assert money.franc(5).currency() == "CHF"
+
+
 def test_equal_same_currency():
     assert money.dollar(5).equals(money.dollar(5))
     assert money.dollar(12).equals(money.dollar(12))
     assert not money.dollar(5).equals(money.dollar(6))
+
     assert money.franc(5).equals(money.franc(5))
     assert money.franc(12).equals(money.franc(12))
     assert not money.franc(5).equals(money.franc(6))
@@ -14,6 +20,7 @@ def test_equal_different_currency():
     assert not money.dollar(5).equals(money.franc(5))
     assert not money.dollar(12).equals(money.franc(12))
     assert not money.dollar(5).equals(money.franc(6))
+
     assert not money.franc(5).equals(money.dollar(5))
     assert not money.franc(12).equals(money.dollar(12))
     assert not money.franc(5).equals(money.dollar(6))
@@ -23,9 +30,7 @@ def test_dolar_mult_by_constant():
     curret_value = money.dollar(5)
 
     assert money.dollar(10).equals(curret_value.times(2))
-
     assert money.dollar(20).equals(curret_value.times(4))
-
     assert money.dollar(35).equals(curret_value.times(7))
 
 
@@ -33,7 +38,5 @@ def test_franc_mult_by_constant():
     curret_value = money.franc(5)
 
     assert money.franc(10).equals(curret_value.times(2))
-
     assert money.franc(20).equals(curret_value.times(4))
-
     assert money.franc(35).equals(curret_value.times(7))
